851a1f4d7573038de94d31aba2f78ae84a8dd050
Siva Muthusamy <s.b.muthusamy@gmail.com>
2017-10-24 07:17:02 -0400

    Tooltips

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -15,17 +15,22 @@ export default class Chart extends React.Component {
 		this.createChart = this.createChart.bind(this)
 	}
 
-
+  componentDidMount() {
+   this.getChart('AAPL', '90m', '1mo')
+  }
+    
+  componentDidUpdate() {
+   this.createChart()
+  }
 
 	getChart(symbol, interval = '1d', range = '1y') {
 		const query = `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?formatted=true&lang=en-US&region=US&interval=${interval}&events=div%7Csplit&range=${range}&corsDomain=finance.yahoo.com`;
 		function round(value, decimals) {
-  			return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+			return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 		}
 		fetch(query)
 			.then(response => response.json())
 			.then(data => {
-				console.log(data)
 				const rounded = data.chart.result[0].indicators.quote[0].close
 				.filter(function(value){
 					return value
@@ -39,143 +44,186 @@ export default class Chart extends React.Component {
 			})
 	}
 
-	componentDidMount() {
-      	this.getChart('AAPL', '90m', '1mo')
-   	}
-   	
-   	componentDidUpdate() {
-   		this.createChart()
-   	}
-   	
-   	createChart() {
-    	var svg = d3.select("svg"),
-   			margin = {top: 10, right: 20, bottom: 30, left: 50},
-    		width = +svg.attr("width") - margin.left - margin.right,
-    		height = +svg.attr("height") - margin.top - margin.bottom,
-    		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  createChart() {
+    const self = this
+    const svg = d3.select("svg"),
+   	margin = {top: 10, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    var parseTime = d3.timeFormat("%B %d, %Y"), 
+    bisectDate = d3.bisector(function(d) { return d.date; }).left,
+    formatValue = d3.format(",.2f"),
+    formatCurrency = function(d) { return "$" + formatValue(d); };
    		
-   		var x = d3.scaleTime()
-    		.rangeRound([0, width]);
+    const x = d3.scaleTime()
+    	.rangeRound([0, width - 2]);
 
-    	var y = d3.scaleLinear()
-    		.rangeRound([height, 0]);
+    const y = d3.scaleLinear()
+    	.rangeRound([height, 10]);
+
+    let testx = x
+    let testy = y
+
+    const zoom = d3.zoom()
+  		.scaleExtent([1, 4])
+  		.translateExtent([[-100, -100], [width + 90, height + 100]])
+  		.on("zoom", zoomed)
 
 
-		var zoom = d3.zoom()
-		    .scaleExtent([1, 3])
-		    .translateExtent([[-100, -100], [width + 90, height + 100]])
-		    .on("zoom", zoomed);
 
+    const line = d3.line()
+    	.x(function(d) { return x(d.date); })
+    	.y(function(d) { return y(d.close); });
 
-    	var area = d3.area()
-    		.x(function(d) { return x(d.date); })
-    		.y0(height)
-    		.y1(function(d) { return y(d.close); });
-
-    	var line = d3.line()
-    		.x(function(d) { return x(d.date); })
-    		.y(function(d) { return y(d.close); });
-
-    	function make_x_gridlines() {		
-    		return d3.axisBottom(x)
-    			.ticks(5)
+    function make_x_gridlines() {		
+    	return d3.axisBottom(x)
+    		.ticks(5);
 		}
 
+  	function make_y_gridlines() {		
+      return d3.axisLeft(y)
+        .ticks(5);
+  	}
 
-		function make_y_gridlines() {		
-    		return d3.axisLeft(y)
-        		.ticks(5)
-		}
+  	function zoomed() {
+  		const e = d3.event;
+  		const tx = Math.min(0, Math.max(e.transform.x, width - width*e.transform.k));
+    	const ty = Math.min(0, Math.max(e.transform.y, height - height*e.transform.k));
+  		dataline.attr("transform", 'translate(' + [tx,ty] + ')scale(' + e.transform.k + ')');
+      focus.attr("transform", 'translate(' + [tx,ty] + ')scale(' + e.transform.k + ')');
+  		const t = d3.zoomIdentity.translate(tx, ty).scale(e.transform.k);
+    	gX.call(xAxis.scale(t.rescaleX(x)));
+    	gY.call(yAxis.scale(t.rescaleY(y)));
+      testx = t.rescaleX(x)
+      testy = t.rescaleY(y)
+    	gridX.call(make_x_gridlines()
+    		.scale(t.rescaleX(x))
+    		.tickSize(-height)
+        .tickFormat(""))
+    	gridY.call(make_y_gridlines()
+    		.scale(t.rescaleY(y))
+    		.tickSize(-width)
+        .tickFormat(""))
+      focus.style("display", "none")
+  	}
+    
+    function mousemove() {
 
-		function zoomed() {
-			let e = d3.event;
-			let tx = Math.min(0, Math.max(e.transform.x, width - width*e.transform.k));
-  			let ty = Math.min(0, Math.max(e.transform.y, height - height*e.transform.k));
-			dataarea.attr("transform", 'translate(' + [tx,ty] + ')scale(' + e.transform.k + ')')
-			dataline.attr("transform", 'translate(' + [tx,ty] + ')scale(' + e.transform.k + ')')
-			console.log([tx, ty, e.transform.k])
-			var t = d3.zoomIdentity.translate(tx, ty).scale(e.transform.k);
-			console.log(t)
-  			gX.call(xAxis.scale(t.rescaleX(x)));
-  			gY.call(yAxis.scale(t.rescaleY(y)));
-		}
+      const x0 = testx.invert(d3.mouse(this)[0]),
+          i = bisectDate(self.state.data, x0, 1),
+          d0 = self.state.data[i - 1],
+          d1 = (self.state.data[i]) ? self.state.data[i] : d0,
+          d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+      focus.attr("transform", "translate(" + testx(d.date) + "," + testy(d.close) + ")");
+      focus.select(".ytooltip").text(formatCurrency(d.close));
+      focus.select(".xtooltip").text(parseTime(d.date));
+      focus.select(".x-hover-line").attr("y2", height - testy(d.close));
+      focus.select(".y-hover-line").attr("x2", -width - testx(d.date) + 890.5);
+      focus.select(".ytooltip").attr("x", -width - testx(d.date) + 843.5);
+      focus.select(".xtooltip").attr("y",height - testy(d.close) + 18);
+    }
 
-		var ydee = d3.extent(this.state.data, function(d) { return d.close; })
-		ydee[1] = ydee[1] + 1
-		console.log(ydee)
-    	x.domain(d3.extent(this.state.data, function(d) { return d.date; }));
-  		y.domain(ydee);
+  	const ydee = d3.extent(this.state.data, function(d) { return d.close; });
 
-  		const xValues = this.state.data.filter((d, i) => i % 32 === 0).map(d => d.date)
+    const xdee = d3.extent(this.state.data, function(d) { return d.date; });
+    console.log(xdee[1])
+    xdee[1] = xdee[1] + 5;
+    console.log(xdee[1])
+    y.domain(ydee);
+    x.domain(d3.extent(this.state.data, function(d) { return d.date; }));
 
-  		var xAxis = d3.axisBottom(x).ticks(5)
+  	const xValues = this.state.data.filter((d, i) => i % 32 === 0).map(d => d.date);
 
-      	var yAxis = d3.axisLeft(y).ticks(5)
+  	const xAxis = d3.axisBottom(x).ticks(5);
+
+    const yAxis = d3.axisLeft(y).ticks(5);
   	
-      	g.append('defs')
-  			.append('clipPath')
-  			.attr('id', 'clip')
-  			.append('rect')
-    		.attr('x', 0)
-    		.attr('y', 0)
-    		.attr('width', width)
-    		.attr('height', height);
+    g.append('defs')
+  		.append('clipPath')
+  		.attr('id', 'clip')
+  		.append('rect')
+    	.attr('x', 0)
+    	.attr('y', 0)
+    	.attr('width', width)
+    	.attr('height', height);
 
-    	const main = g.append('g')
-      	.attr('class', 'main')
-      	.attr('clip-path', 'url(#clip)');
+    const main = g.append('g')
+      .attr('class', 'main')
+      .attr('clip-path', 'url(#clip)')
 
-    	var view = main.append("rect")
-    				.attr("class", "view")
-   				 	.attr("x", 0.5)
-    				.attr("y", 0.5)
-    				.attr("width", width)
-    				.attr("height", height)
-    				.call(zoom);
+    const view = main.append("rect")
+    	.attr("class", "view")
+   		.attr("x", 0.5)
+    	.attr("y", 0.5)
+    	.attr("width", width)
+    	.attr("height", height)
 
-    	main.append("g")			
-      		.attr("class", "grid")
-      		.attr("transform", "translate(0," + height + ")")
-      		.call(make_x_gridlines()
-          		.tickSize(-height)
-          		.tickFormat("")
-      		)	
+    const gridX = main.append("g")			
+      .attr("class", "grid")
+      .attr("transform", "translate(0," + height + ")")
+      .call(make_x_gridlines()
+          .tickSize(-height)
+          .tickFormat(""));	
 
   
-  		main.append("g")			
-      		.attr("class", "grid")
-      		.call(make_y_gridlines()
-          		.tickSize(-width)
-          		.tickFormat("")
-      		)
+  	const gridY = main.append("g")			
+      .attr("class", "grid")
+      .call(make_y_gridlines()
+        .tickSize(-width)
+        .tickFormat(""));
   		
-  		var gX = g.append("g")
-      		.attr("transform", "translate(0," + height + ")")
-      		.attr("class", "axis")
-      		.call(xAxis)
+  	const gX = g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .attr("class", "axis")
+      .call(xAxis);
 
 
-	  	var gY = g.append("g")
-	  		.attr("class", "axis")
-	      	.call(yAxis)
+	  const gY = g.append("g")
+      .attr("class", "axis")
+      .call(yAxis);
 
-	  	var dataarea = main.append("path")
-	  		.datum(this.state.data)
-	       	.attr("fill", "#99FFCE")
-	       	.attr("stroke-width", 0)
-	       	.attr("d", area);
+	  const dataline = main.append("path")
+        .datum(this.state.data)
+	      .attr("fill", "none")
+	      .attr("stroke", "#00E676")
+	      .attr("stroke-linejoin", "round")
+	      .attr("stroke-linecap", "round")
+	      .attr("stroke-width", 3)
+	      .attr("d", line);
 
+    const focus = g.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
 
-	  	var dataline = main.append("path")
-	      	.datum(this.state.data)
-	      	.attr("fill", "none")
-	      	.attr("stroke", "#00E676")
-	      	.attr("stroke-linejoin", "round")
-	      	.attr("stroke-linecap", "round")
-	      	.attr("stroke-width", 3)
-	      	.attr("d", line);
+    focus.append("circle")
+      .attr("r", 4.5);
 
-	    g.call(zoom)
+    focus.append("text")
+      .attr("class", "ytooltip")
+      .attr("dy", ".35em")
+
+    focus.append("text")
+      .attr("class", "xtooltip")
+      .attr("dx", "-3.5em")
+
+    focus.append("line")
+        .attr("class", "x-hover-line hover-line")
+        .attr("y1", 0)
+        .attr("y2", height);
+
+    focus.append("line")
+        .attr("class", "y-hover-line hover-line")
+        .attr("x1", 0)
+        .attr("x2", width);
+
+    g.on("mousemove",  mousemove)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .call(zoom)
+
 }
 
 
@@ -183,7 +231,7 @@ export default class Chart extends React.Component {
 		return (
 			<svg ref={node => this.node = node}
       		width={960} height={500}>
-      		</svg>
+      	</svg>
 		)
 	}
 }
